3872a5e7b22e4c18492a1cbb22bf768f0bd5b057
Marc Weinberger <marc.weinberger@me.com>
2011-07-21 23:33:46 +0200

    loading twitter lists from cache

--- app.rb
+++ app.rb
@@ -2,22 +2,25 @@ require 'rubygems'
 require 'sinatra'
 require "sinatra/reloader" if development?
 require 'twitter'
+require "redis"
 require 'haml'
+require 'lib/twitter_stream'
 
-helpers do
-  def followers screen_name
-    id = Twitter.user(screen_name).id
-    follower_ids = Twitter.follower_ids(id)
-    Twitter.users(Array(follower_ids))
-  end
+set :public, File.dirname(__FILE__) + '/public'
+
+configure do
+  # initialize redis connection
+  @@redis = Redis.new(:host => 'localhost', :port => 6379)
+  
+  # initialize twitter stream
+  @@stream = TwitterStream.new
+   @@stream.lists.each do |list|
+     @@redis.sadd('user:marc:lists', list.name)
+   end
 end
 
 get '/' do
+  @lists = @@redis.smembers('user:marc:lists')
   haml :index
-end
-
-get '/followers' do
-  @followers = followers("mixomat")
-  haml :following
 end
 
