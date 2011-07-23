4b85d9004f28c5b5144b9bf17b5a89c224bd886a
Marc Weinberger <marc.weinberger@me.com>
2011-07-23 02:29:34 +0200

    renamed list_spec

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -8,11 +8,19 @@ describe TwitterStream::Lists do
   
   it "can instantiate with user" do
     lists = TwitterStream::Lists.new("mixomat")
+    
+    lists.user.should === "mixomat"
   end
   
-  it "can retrieve lists for user " do
-    lists = @twitter_lists.lists
+  it "can update the users lists from twitter" do
+    lists = @twitter_lists.update
     
+    lists.first.should be_kind_of Hashie::Mash
+  end
+  
+  it "can retrieve cached lists for user " do
+    lists = @twitter_lists.load
+
     lists.should be_kind_of Array
     lists.should have_at_least(1).things
     lists.first[:name].should === "Games"
@@ -22,7 +30,9 @@ describe TwitterStream::Lists do
     list = @twitter_lists.lists.first
     timeline = @twitter_lists.timeline list
     
+    timeline.should have_at_least(20).thing
     timeline.first.should be_kind_of Hashie::Mash
+    timeline.first.text.should have_at_least(2).characters
   end
   
 
