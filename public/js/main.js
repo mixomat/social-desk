e4a8e9c5853451fb3319334c0d7d275bbff3fb7d
Marc Weinberger <marc.weinberger@me.com>
2011-07-26 19:04:17 +0200

    working on backbone model for user lists and timeline

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,38 @@
 require(["jquery", "underscore-min", "backbone-min" ], function($) {
 	
+	var Tweet = Backbone.Model.extend({});
+	
+	var Timeline = Backbone.Collection.extend({
+		model: Tweet,
+		url: function () {
+			'/lists/' + this.get['id'] + '/timeline'
+		} 
+	});
+	
+	var timeline = new Timeline();
+	
+	
+	var TimelineView = Backbone.View.extend({
+		
+		events : {
+			"click option": "timeline"
+		},
+
+		initialize : function() {
+			_.bindAll(this, "render", "timeline");
+		},	
+		
+		timeline: function() {
+			alert("switching timeline");
+		},
+		
+		render: function() {
+			console.log("render called");
+		}
+		
+		
+	});
+	
+	window.app = new TimelineView();
+	console.log(window.app);
 });
